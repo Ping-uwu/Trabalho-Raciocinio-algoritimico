4e8afe37adf803d02c983542d17eed9ef860d94e
Ping-uwu <130710871+Ping-uwu@users.noreply.github.com>
2023-04-13 20:09:12 -0300

    Update Maquina de refri.py

--- Maquina de refri.py
+++ Maquina de refri.py
@@ -4,7 +4,7 @@ while True:
     print("Se deseja sair digite: exit")
     escolha_produto = (input("Digite aqui o código do produto que deseja: "))
 
-    if escolha_produto == "A1":
+    if escolha_produto == "A1" or "a1":
         coca = 4.0
         print("Sua escolha custa 4.00 R$")
         cédulas2 = 2.0 * int(input("Quantas cedulas de 2 você inseri: "))
@@ -21,10 +21,10 @@ while True:
             valorbebida = (valorpago - coca) 
             print("Sua bebida foi dispejada e seu troco é de:", valorbebida, "R$ por favor os retires de seu recipiente de entrega apropriado e tenha um bom dia.")
             exit()
-    elif escolha_produto == "ADM":
+    elif escolha_produto == "ADM" or "adm":
         print("Bem-Vindo chefe, como podemos ajuda-lo hoje")
         print(" 1 Colocar dinheiro na maquina")
-    elif escolha_produto == "exit":
+    elif escolha_produto == "exit" or "Exit":
         exit()
     else:
         print("Opção não compreendida por favor digite sua opção novamente.")
